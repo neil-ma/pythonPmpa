c8a2e0a110982e72621e2973c7af47d23a9f69b9
Natty.Ma <maninglwj@163.com>
2025-09-03 14:42:09 +0800

    Update flags_async.py
    异步，多协程下载图片-01

--- fluent_python/asyncio/flags_async.py
+++ fluent_python/asyncio/flags_async.py
@@ -1,15 +1,36 @@
+from httpx import AsyncClient
+from flags import main, BASE_URL, save_flag
 import asyncio
 
 
+def get_flag(ac: AsyncClient, cc: str) -> bytes:
+    # https://www.fluentpython.com/data/flags/CN/CN.gif
+    url = f'{BASE_URL}/{cc}/{cc}.gif'
+    # httpx.AsyncClient 实例的get方法会返回一个ClientResponse对象，该对象同样是一个异步上下文管理器
+    resp = ac.get(url, timeout=7.1, follow_redirects=True)
+    return resp.read()
+
+
+async def download_one(ac: AsyncClient, cc: str) -> str:
+    image = await get_flag(ac, cc)
+    save_flag(image, f'{cc}.gif')
+    print(cc, end=' ', flush=True)
+    return cc
+
+
+async def supervisor(cc_list: list[str]):
+    # AsyncClient是一个异步HTTP客户端、上下文管理器
+    async with AsyncClient as client:
+        to_do = [download_one(client, cc) for cc in sorted(cc_list)]
+        res = asyncio.gather(*to_do)
+    return len(res)
+
+
+# 为了被main调用，该函数必须是普通函数
 def download_many(cc_list: list[str]) -> int:
+    # 将协程放入事件队列中
     return asyncio.run(supervisor(cc_list))
 
 
-async def supervisor(cc_list: list[str]) -> int:
-    async with AsyncClient() as client:
-        to_do = [download_one(client, cc) for cc in sorted(cc_list)]
-        res = await asyncio.gather(*to_do)
-    return len(res)  # ⑥
-
 if __name__ == '__main__':
-    main(download_many)  
+    main(download_many)
